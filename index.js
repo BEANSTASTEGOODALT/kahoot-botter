c575e019e106adce2f2aa616e99de8a499f0a69c
BEANSTASTEGOODALT <hudsonhippo123@gmail.com>
2025-11-20 16:07:25 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const Kahoot = require("kahoot.js-updated");
+const Kahoot = require("kahoot.js-updated-fixed");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
